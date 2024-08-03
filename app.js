b4ededc788d7db5a5d7ed227976dc71482f69f35
Faiz <faizkhan8225@gmail.com>
2024-08-03 16:52:26 +0530

    Secding request response and able to create a post.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express")
+const bodyPardser = require("body-parser")
 
 const feedRoutes = require("./routes/feed")
 
 const app = express()
+
+// app.use(bodyPardser.urlencoded()) // x-www-form-urlencoded <form>
+app.use(bodyPardser.json()) // application/json
 
 app.use("/feed", feedRoutes)
 
--- routes/feed.js
+++ routes/feed.js
@@ -7,4 +7,7 @@ const router = epxress.Router()
 // GET /feed/posts
 router.get("/posts", feedController.getPosts)
 
+// POST /feed/post
+router.post("/post", feedController.createPost)
+
 module.exports = router
